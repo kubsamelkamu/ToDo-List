9c40336e691876d8f6259370a597ead5c4ecc7ff
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-30 17:14:38 +0300

    toggle method

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,3 +1,8 @@
 function TaskListComponent(tasks,setTasks) {
 
+    const handleToggleCompletes =(index)=>{
+        const newTasks = [...tasks];
+        newTasks[index].completed = !newTasks[index].completed;
+        setTasks(newTasks);
+    }
 }
