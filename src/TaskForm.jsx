13cf9a3a6437cbb0c5eaf0c53ba6e4a8a9aaf165
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-30 16:30:52 +0300

    added form element

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -3,5 +3,11 @@ const { useState } = require("react");
 function TaskManager(){
     const[input,setInput] = useState('');
 
-    
+    return(
+        <form onSubmit={}>
+            <input type="text" id="tasks" value={input} 
+            placeholder="Enter tasks ..." onChange={(e)=>e.target.value}/>
+            <button type="submit">ADD</button>
+        </form>
+    )
 }
