d703837acdfffe6dabfee21c8f53d91d919a72fc
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-31 09:39:05 +0300

    div container

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -5,4 +5,24 @@ function TaskListComponent(tasks,setTasks) {
         newTasks[index].completed = !newTasks[index].completed;
         setTasks(newTasks);
     }
+
+    const handleDelete=()=>{
+        setTasks(tasks.filter((_, i) => i !== index));
+    }
+
+    return(
+        <div className="task-list">
+            <ul>
+                {tasks.map((task,index)=>(
+                     <li key={index} className={task.completed ? 'completed' : ''}>
+                        <span onClick={() => handleToggleCompletes(index)}>
+                        {task.text}
+                        </span>
+                        <button type="button" onClick={() => handleDelete(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        
+        </div>
+    )
 }
